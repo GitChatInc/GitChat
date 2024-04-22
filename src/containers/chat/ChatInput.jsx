dd30f866d62df44a008932777038d4cc72c603f1
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-22 19:49:20 -0400

    Scaffold ChatMain container

--- src/containers/chat/ChatInput.jsx
+++ src/containers/chat/ChatInput.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default () => {
   return (
-    <div class='border'>
-      ChatInput
+    <div class='border h-1/5'>
+      <form action="">
+        <input type="text" />
+        <button>Send</button>
+      </form>
     </div>
   );
 };
--- src/containers/chat/ChatMessageLog.jsx
+++ src/containers/chat/ChatMessageLog.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Message from '../../components/Message.jsx';
 
 export default () => {
-  return (
-    <div class='border'>
-      ChatMessageLog
-    </div>
-  );
+  const testMessages = [
+    { from: 'marsbird', content: 'birds r cool', timestamp: 'x' },
+    { from: 'tenn501', content: 'jiggly wiggly', timestamp: 'x' },
+    { from: 'emmagawd', content: 'bro', timestamp: 'x' },
+  ];
+  let messages = [];
+  testMessages.forEach((e, i) => {
+    messages.push(<Message key={i} from={e.from} content={e.content} />);
+  });
+
+  return <div class='border h-4/5'>{messages}</div>;
 };
