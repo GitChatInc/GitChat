bc350c68b1a15f5d5ecbb71188ded0742718ac0e
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-22 16:22:41 -0400

    Intialize repo with basic dependencies and webpack
    Co-authored-by: Congke Zhao <CK-Zhao@users.noreply.github.com>
    Co-authored-by: Bongi Sibanda <trialnerr@users.noreply.github.com>
    Co-authored-by: Emma Ijiogbe <emmagawd@users.noreply.github.com>
    Co-authored-by: Lillian Tenn <tenn501@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+if (process.env.NODE_ENV === 'development') {
+  //get files from src during dev
+  app.use(express.static(path.resolve(__dirname, '../src')));
+} else {
+  //get files from dist during production
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+}
+
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
+
+module.exports = app;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <header>
+        <h1>Welcome to gitChat</h1>
+      </header>
+    </main>
+  );
+};
+
+export default App;
