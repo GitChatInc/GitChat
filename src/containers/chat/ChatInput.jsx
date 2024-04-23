a37b56a2ea491f8cb3f8620a4799be976af3e61d
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 08:44:40 -0400

    Adjust formatting on chatInput

--- src/containers/chat/ChatInput.jsx
+++ src/containers/chat/ChatInput.jsx
@@ -4,11 +4,8 @@ export default () => {
   return (
     <div className="p-2">
       <form action="" className="flex">
-        <input
-          type="text"
-          className="w-full rounded-lg bg-slate-600 p-1"
-        />
-        <button className="ml-3 rounded-lg bg-slate-500 py-1 px-4">Send</button>
+        <input type="text" className="w-full rounded-lg bg-slate-600 p-1" />
+        <button className="ml-3 rounded-lg bg-slate-500 px-4 py-1">Send</button>
       </form>
     </div>
   );
