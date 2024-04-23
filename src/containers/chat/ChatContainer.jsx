20da7de16689817be2ef18a6c107c5484a36a76c
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-22 23:02:15 -0400

    Set currentRepo default to null

--- src/containers/chat/ChatContainer.jsx
+++ src/containers/chat/ChatContainer.jsx
@@ -3,7 +3,7 @@ import ChatSidebar from "./ChatSidebar.jsx";
 import ChatMain from "./ChatMain.jsx";
 
 export default () => {
-  const [currentRepo,setCurrentRepo] = useState('hello')
+  const [currentRepo,setCurrentRepo] = useState(null)
 
   return (
     <div className="flex h-[700px] w-1/2 rounded-2xl bg-slate-800">
