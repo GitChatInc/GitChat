358f33f9b05d3f3fa0a8569a03f81901ac4d6960
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-23 15:50:45 -0400

    working on messageController

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,10 +1,16 @@
-
 const MessageController = {};
-
+const db = require('../models/model.js');
 
 MessageController.getMessages = (req, res, next) => {
+  // retrieve repo id from params;
+  // const { id } = req.params;
+  // console.log({id});
+  // const query = ``;
+
   // query db for all messages related to the repo id;
-}
+    
+  // persist the list through middleware to send back to client;
+};
 
 MessageController.addMessage = (req, res, next) => {
   // retrieve repo id from params, send a post
