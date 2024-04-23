0c99babd572c5acdcccb2a50fc46bcd5b28d0b4a
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 11:44:40 -0400

    Create simple Auth component

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ChatContainer from "./chat/ChatContainer.jsx";
+import LogInContainer from "./auth/AuthContainer.jsx";
 
 export default () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900 text-slate-400">
-      <ChatContainer />
+      {/* <ChatContainer /> */}
+      <LogInContainer />
     </div>
   );
 };
---
+++ src/containers/auth/AuthContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="flex h-96 w-1/3 flex-col items-center justify-center rounded-2xl bg-slate-800">
+      <h1 className="text-3xl">GitChat</h1>
+      <div>Log In Here!</div>
+    </div>
+  );
+};
