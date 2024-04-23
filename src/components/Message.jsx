7e96db7b86fe732842ef1584fb27cc3be628b416
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 01:27:21 -0400

    Remove sender elements on sequential messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,16 +4,16 @@ export default (props) => {
   return (
     <div className="mx-3 mb-3 flex items-end">
       {/* First Inital of the Sender for Use in case of no picture available */}
-      {/* <span className="mr-3 flex h-8 w-8 items-center justify-center rounded-lg bg-slate-600 text-xl">
+      {/* <span className="mr-3 flex h-8 w-8 items-ce`nter justify-center rounded-lg bg-slate-600 text-xl">
         {props.from[0].toUpperCase()}
       </span> */}
       <img
         src={`https://github.com/${props.from}.png`}
         alt={`${props.from}'s GitHub profile picture`}
-        className="mr-3 flex h-8 w-8 items-center justify-center rounded-lg bg-slate-600 text-xl brightness-75"
+        className={`mr-3 flex h-8 w-8 items-center justify-center rounded-lg bg-slate-600 ${props.seq ? "invisible" : "visible"} text-xl brightness-75`}
       />
       <div>
-        {props.from}
+        {props.seq ? "" : props.from}
         <div className="w-fit max-w-xs rounded-lg bg-slate-900 p-2">
           {props.content}
         </div>
--- src/containers/chat/ChatMessageLog.jsx
+++ src/containers/chat/ChatMessageLog.jsx
@@ -2,67 +2,87 @@ import React from "react";
 import Message from "../../components/Message.jsx";
 
 export default (props) => {
-  const testMessages = [
-    { from: "emmagawd", content: "bro", timestamp: "x", repo: "gitchat" },
+  let testMessages = [
+    { from: "emmagawd", content: "bro", timestamp: "10", repo: "gitchat" },
     {
       from: "tenn501",
       content:
         "React will create a root for the domNode, and take over managing the DOM inside it. After you’ve created a root, you need to call root.render to display a React component inside of it:",
-      timestamp: "x",
-      repo: "gitchat",
-    },
-    {
-      from: "tenn501",
-      content: "jiggly wiggly",
-      timestamp: "x",
+      timestamp: "9",
       repo: "gitchat",
     },
     {
       from: "CK-Zhao",
       content: "me too!",
-      timestamp: "x",
+      timestamp: "7",
+      repo: "gitchat",
+    },
+    {
+      from: "tenn501",
+      content: "jiggly wiggly",
+      timestamp: "8",
       repo: "gitchat",
     },
     {
       from: "trialnerr",
       content: "I <3 my wife",
-      timestamp: "x",
+      timestamp: "6",
       repo: "gitchat",
     },
     {
       from: "marsbird",
       content: "birds r cool",
-      timestamp: "x",
+      timestamp: "4",
       repo: "gitchat",
     },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
-    { from: "tenn501", content: "buzz", timestamp: "x", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "1", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "2", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "3", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "4", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "5", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "6", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "7", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "8", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "9", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "10", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "11", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "12", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "13", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "14", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "15", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "16", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "17", repo: "solibee" },
+    { from: "tenn501", content: "buzz", timestamp: "18", repo: "solibee" },
   ];
-  let messages = [];
+
+
+  // messages need to be filtered, sorted, flagged for sequential senders and then sorted in reverse before converted to components
+  let prevMessageSender;
+
+  testMessages = testMessages.filter((message) => {
+    return message.repo == props.currentRepo;
+  });
+
+  testMessages.sort((a, b) => a.timestamp - b.timestamp);
+
+  testMessages.forEach((message) => {
+    message.from == prevMessageSender
+      ? (message.seq = true)
+      : (message.seq = false);
+    prevMessageSender = message.from;
+  });
+
+  testMessages.sort((a, b) => b.timestamp - a.timestamp);
+
+  const messages = [];
   testMessages.forEach((e, i) => {
-    if (e.repo == props.currentRepo) {
-      messages.push(<Message key={i} from={e.from} content={e.content} />);
-    }
+    messages.push(
+      <Message key={i} from={e.from} content={e.content} seq={e.seq} />,
+    );
   });
 
   return (
-    <div className="overflow-y-hidden hover:overflow-y-auto flex flex-col-reverse">
+    <div className="flex flex-col-reverse overflow-y-hidden hover:overflow-y-auto">
       {messages}
     </div>
   );
