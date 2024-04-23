8f31a227c8932b1b3662af567ce8569c942b5f6a
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 09:04:26 -0400

    Change input to textarea

--- src/containers/chat/ChatInput.jsx
+++ src/containers/chat/ChatInput.jsx
@@ -4,7 +4,10 @@ export default () => {
   return (
     <div className="p-2">
       <form action="" className="flex">
-        <input type="text" className="w-full rounded-lg bg-slate-600 p-1" />
+        <textarea
+          className="h-fit w-full resize-none rounded-lg bg-slate-600 p-1"
+          type="submit"
+        />
         <button className="ml-3 rounded-lg bg-slate-500 px-4 py-1">Send</button>
       </form>
     </div>
