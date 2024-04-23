fb085617d8c1adf408eacae6f5683ddc1a8e7864
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 15:11:15 -0400

    Set up react router between auth and chat pages

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ChatContainer from "./chat/ChatContainer.jsx";
 import LogInContainer from "./auth/AuthContainer.jsx";
+import { Routes, Route } from "react-router-dom";
 
 export default () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900 text-slate-400">
-      {/* <ChatContainer /> */}
-      <LogInContainer />
+      <Routes>
+        <Route path="/" element={<LogInContainer />} />
+        <Route path="/chat" element={<ChatContainer />} />
+      </Routes>
     </div>
   );
 };
