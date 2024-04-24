c908f143a74f56afaf33db3caa812729f5d1e91d
Marselena Sequoia <109041187+marsbird@users.noreply.github.com>
2024-04-23 22:43:56 -0400

    Make Auth container responsive and mobile friendly

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ChatContainer from "./chat/ChatContainer.jsx";
-import LogInContainer from "./auth/AuthContainer.jsx";
+import AuthContainer from "./auth/AuthContainer.jsx";
 import { Routes, Route } from "react-router-dom";
 
 export default () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-slate-900 text-slate-400">
       <Routes>
-        <Route path="/" element={<LogInContainer />} />
+        <Route path="/" element={<AuthContainer />} />
         <Route path="/chat" element={<ChatContainer />} />
       </Routes>
     </div>
