123ab356c183ff602dc0c136c891ab9e563c3173
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-04-24 10:13:41 -0400

    Update middleware routing for reposController

--- server/routers/reposRouter.js
+++ server/routers/reposRouter.js
@@ -5,14 +5,14 @@ const MessageController = require ('../controllers/messageController.js');
 
 
 //get all repos for a user
-ReposRouter.get('/repos', ReposController.repoList, (req, res, next) => {
+ReposRouter.get('/repos/:userId', ReposController.getUserRepoList, (req, res, next) => {
   
-  return res.status(200);
+  return res.status(200).json(res.locals.userRepos);
 });
 
 ReposRouter.post('/repos', ReposController.addRepos, (req, res, next) => {
     
-  return res.status(201).json(res.locals.addedRepo);
+  return res.status(201).json(res.locals.addedRepos);
 
 });
 
